6ac84525b0d9bcf00f8ba184efe4d18be7307e85
RoxiFOchoaG <46870050+roxifochoag@users.noreply.github.com>
2023-05-08 11:37:49 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import validator from './validator.js';
 
-console.log(validator);
+const validateForm = document.getElementById('validateForm');
+const cardInput = document.getElementById('cardNumberInput');
+const nameInput = document.getElementById('clientName');
+const resultBox = document.getElementById('result');
+const cardImgNumber = document.getElementById('creditCardNumber');
+const cardImgName = document.getElementById('creditCardName');
+
+nameInput.addEventListener('keyup', () => {
+  if (nameInput.value === '') {
+    cardImgName.innerHTML = 'NOMBRE';
+  } else {
+    cardImgName.innerHTML = nameInput.value.toUpperCase();
+  }
+});
+
+cardInput.addEventListener('keyup', () => {
+  let formattedText = cardInput.value.split(' ').join('');
+    if (formattedText.length <= 16) {
+      formattedText.length > 0 ? formattedText = formattedText.match(new RegExp('.{1,4}', 'g')).join(' ') : alert('Detente aquí')
+    }
+  cardImgNumber.innerHTML = validator.maskify(formattedText);
+})
+
+validateForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let cardNumber = cardInput.value;
+  let validateResult = validator.isValid(cardNumber);
+  if(validateResult) {
+    resultBox.textContent = 'La tarjeta es válida';
+  } else {
+    resultBox.textContent = 'La tarjeta no es válida';
+  }
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,36 @@
 const validator = {
-  // ...
+  isValid: (creditCardNumber) => {
+    let cardArr = creditCardNumber.split("").reverse();
+    let totalAdd = [];
+    for (let i = 0; i < cardArr.length; i++) {
+      if (i % 2 == 0) {
+        totalAdd.push(parseInt(cardArr[i]))
+      } else {
+        cardArr[i] = parseInt(cardArr[i]) * 2;
+        if (cardArr[i] > 9) {
+          let num = cardArr[i].toString().split("");
+          totalAdd.push(parseInt(num[0]) + parseInt(num[1]));
+        } else {
+          totalAdd.push(parseInt(cardArr[i]));
+        }
+      }
+    }
+    const reducedArr = totalAdd.reduce((previousValue, currentValue) => previousValue + currentValue);
+    if (reducedArr % 10 === 0) {
+      return true;
+    }
+    return false;
+  },
+  maskify: (creditCardNumber) => {
+    const end = creditCardNumber.slice(-4);
+    let replaced = '';
+    for (let i = 0; i < creditCardNumber.length - 4; i++) {
+      replaced += creditCardNumber[i].replace(/\w/g, "#");
+    }
+    return replaced + end;
+  },
+
 };
 
 export default validator;
+
